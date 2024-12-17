13ce0ba9fae20ca328aefbd5b3e16e8b1218491f
tomohiro.jin <tomohiro.jin@hotmail.co.jp>
2024-12-17 13:44:28 +0000

    feat: 基本的なテストが動作するところまで作成

---
+++ jest.config.js
@@ -0,0 +1,4 @@
+export const preset = 'ts-jest';
+export const testEnvironment = 'node';
+export const testMatch = ["**/tests/**/*.test.ts", "**/src/**/*.test.ts"]
+export const cacheDirectory = "node_modules/.cache/jest"
