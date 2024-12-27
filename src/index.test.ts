6cf42e2759944e36033798714420d27fb82f7668
tomohirojin <tomohiro.jin@gmail.com>
2024-12-27 13:55:50 +0000

    wip: 一旦なんとなく人生のコミットを打つ
    Co-authored-by: kfjt <kfjt@users.noreply.github.com>
    Co-authored-by: terahide <terahide@users.noreply.github.com>
    Co-authored-by: omi-nosuke <omi-nosuke@users.noreply.github.com>
    Co-authored-by: ENJOJI Yasuto  <yasuto.enjoji@gmail.com>
    Co-authored-by: yattom <tsutomu.yasui@gmail.com>
    Co-authored-by: Youichi Takigawa <takigawa.401@gmail.com>
    Co-authored-by: Akira Kubo <akirakubo-km@users.noreply.github.com>

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,60 @@
-import { add } from "./index";
+import { isCorrectOrders } from ".";
 
-test("add function", () => {
-  expect(add(2, 3)).toBe(5);
-  expect(add(-1, 1)).toBe(0);
-  expect(add(0, 0)).toBe(0);
+describe("Day 5", () => {
+  describe("更新(ページリスト)を渡すと、正しい順序か判定できる", () => {
+    describe("更新(ページリスト)1行とルール1行を比べる", () => {
+      it("ルール通りに値がある場合ならtrue", () => {
+        expect(isCorrectOrders([75, 47, 61, 53, 29], [[47, 53]])).toBeTruthy();
+      });
+      it("ルールに従っていない（ページの順番が間違っている）場合ならfalse", () => {
+        expect(isCorrectOrders([75, 47, 61, 53, 29], [[53, 47]])).toBeFalsy();
+      });
+      it("ルールのページ番号がどちらも登場しないならtrue", () => {
+        expect(isCorrectOrders([75, 47, 61, 53, 29], [[11, 12]])).toBeTruthy();
+      });
+      it("ルールの一番目のページ番号が登場しないならtrue", () => {
+        expect(isCorrectOrders([75, 47, 61, 53, 29], [[11, 29]])).toBeTruthy();
+      });
+      it("ルールの弐番目のページ番号が登場しないならtrue", () => {
+        expect(isCorrectOrders([75, 47, 61, 53, 29], [[75, 12]])).toBeTruthy();
+      });
+    });
+    describe("更新(ページリスト)1行とルール複数行を比べる", () => {
+      it("すべてのルール通りに値がある場合ならtrue", () => {
+        expect(
+          isCorrectOrders(
+            [75, 47, 61, 53, 29],
+            [
+              [47, 53],
+              [47, 29],
+            ]
+          )
+        ).toBeTruthy();
+      });
+
+      it("一番目のルールが従っていない場合ならfalse", () => {
+        expect(
+          isCorrectOrders(
+            [75, 47, 61, 53, 29],
+            [
+              [53, 47],
+              [47, 29],
+            ]
+          )
+        ).toBeFalsy();
+      });
+
+      it("弐番目のルールが従っていない場合ならfalse", () => {
+        expect(
+          isCorrectOrders(
+            [75, 47, 61, 53, 29],
+            [
+              [47, 53],
+              [29, 47],
+            ]
+          )
+        ).toBeFalsy();
+      });
+    });
+  });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
-export const add = (a: number, b: number): number => {
-  return a + b;
+const _isCorrectOrder = (pageNumbers: number[], rule: number[]): boolean => {
+  const findIndex = (ruleNumber: number) =>
+    pageNumbers.findIndex((n) => n === rule[ruleNumber]);
+  const secondIndex = findIndex(1);
+  if (secondIndex < 0) return true;
+  return pageNumbers.findIndex((n) => n === rule[0]) <= secondIndex;
+};
+
+export const isCorrectOrders = (
+  pageNumbers: number[],
+  rules: number[][]
+): boolean => {
+  return rules.every((r) => _isCorrectOrder(pageNumbers, r));
 };
