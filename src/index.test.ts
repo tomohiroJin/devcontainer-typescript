2aaafcdcec46c27b12260e87b8b1aceaed7f333f
tomohirojin <tomohiro.jin@gmail.com>
2026-05-14 17:50:12 +0000

    add: 法則がわからないので三角測量を追加

--- src/index.test.ts
+++ src/index.test.ts
@@ -4,3 +4,8 @@ test('長さ5のカウンターストリングの結果として35*が返る', (
   const actual = generateCounterString(5);
   expect(actual).toBe('35*');
 });
+
+test('長さ10のカウンターストリングの結果として35710*が返る', () => {
+  const actual = generateCounterString(10);
+  expect(actual).toBe('35710*');
+});
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export const generateCounterString = (_length: number): string => {
-  return '35*';
+  if (_length === 5) return '35*';
+
+  return '35710*';
 };
