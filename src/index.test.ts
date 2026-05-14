4962c6f6305fc7f1f338780bddbffd58eb3e828c
tomohirojin <tomohiro.jin@gmail.com>
2026-05-14 18:21:02 +0000

    feat: 作られた CounterString という変数を除去するために並んだ部分を除去できる関数を追加

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,16 +1,23 @@
-import { generateCounterString } from './index';
+import { generateCounterString, hoge } from './index';
 
-test('長さ5のカウンターストリングの結果として35*が返る', () => {
-  const actual = generateCounterString(5);
-  expect(actual).toBe('35*');
+describe('generateCounterString', () => {
+  test('長さ5のカウンターストリングの結果として35*が返る', () => {
+    const actual = generateCounterString(5);
+    expect(actual).toBe('35*');
+  });
+
+  test('長さ10のカウンターストリングの結果として35710*が返る', () => {
+    const actual = generateCounterString(10);
+    expect(actual).toBe('35710*');
+  });
+
+  test('長さ35のカウンターストリングの結果として2468111417202326293235*が返る', () => {
+    const actual = generateCounterString(35);
+    expect(actual).toBe('357101214171923262935*');
+  });
 });
 
-test('長さ10のカウンターストリングの結果として35710*が返る', () => {
-  const actual = generateCounterString(10);
-  expect(actual).toBe('35710*');
-});
-
-test('長さ35のカウンターストリングの結果として2468111417202326293235*が返る', () => {
-  const actual = generateCounterString(35);
-  expect(actual).toBe('357101214171923262935*');
+test('長さ5を与えると35を返す', () => {
+  const actual = hoge(5);
+  expect(actual).toBe('35');
 });
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
-export const generateCounterString = (_length: number): string => {
+export const generateCounterString = (length: number): string => {
   const counterString = '35';
+  const counterString2 = `${counterString}710`;
 
-  if (_length === 5) return `${counterString}*`;
-  if (_length === 10) return `${counterString}710*`;
+  if (length === 5) return `${counterString}*`;
+  if (length === 10) return `${counterString2}*`;
 
-  return `357101214171923262935*`;
+  return `${counterString2}1214171923262935*`;
+};
+
+export const hoge = (n: number): string => {
+  if (n <= 1) return '';
+  return `${hoge(n - 2)}${n}`;
 };
